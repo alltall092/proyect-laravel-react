153d1f9e49923c2c8abe84b26c915ca8458513ac
julio alberto perez <piyoyo279@gmail.com>
2023-08-09 18:08:46 -0400

    laravel-react

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     build: {
         manifest: true,
-        outDir: 'public/build',
+        outDir: 'public/dist',
         rollupOptions: {
             input: 'resources/js/app.jsx',
         },
