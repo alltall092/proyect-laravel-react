f04dfae72302bb0cc3b0f8d480b64af3cbeeb3e6
julio alberto perez <piyoyo279@gmail.com>
2023-08-10 16:28:21 -0400

    laravel-react

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
- 
-export default defineConfig({
-  
+import reactRefresh from '@vitejs/plugin-react-refresh';
+
+export default ({ command }) => ({
+    base: command === 'serve' ? '' : '/build/',
+    publicDir: 'fake_dir_so_nothing_gets_copied',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.jsx',
+        },
+    },
+
     plugins: [
         laravel(['resources/js/app.jsx']),
         react(),
+        reactRefresh(),
+        defineConfig()
+
     ],
 });
